abeef2a9e53c56864d6e8042dd6a230ecdeec46b
giogiosgr <giovannisgr88@gmail.com>
2024-05-16 20:40:20 +0200

    feat: snack3

--- snack2/README.md
+++
@@ -1,2 +0,0 @@
-
-#### Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
--- snack3/JS/main.js
+++ snack3/JS/main.js
@@ -6,8 +6,8 @@ let somma = 0;
 //iniziamo il ciclo for per 10 iterazioni
 for (let i = 1; i <= 10; i++) {
     //ad ogni iterazione aggiungiamo alla variabile somma il nuovo valore (convertito da String a Number)    
-    somma += +prompt(`inserisci il numero in posizione ${i}`);
+    somma += +prompt(`inserisci ${i}° numero`);
 }
 
 // stampiamo la somma
-console.log(somma)
+console.log(somma);
