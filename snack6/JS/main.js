dd03e87960c4e23345de129cd76621d537463745
giogiosgr <giovannisgr88@gmail.com>
2024-05-17 09:40:02 +0200

    feat: snack6

--- snack6/JS/main.js
+++ snack6/JS/main.js
@@ -1,1 +1,39 @@
 'use strict';
+
+//PERSONALE VARIAZIONE DELL'ESERCIZIO
+//L'utente ha tre tentativi invece di uno
+
+//inizializzo variabile che conterrà l'input utente
+let guess = 0;
+
+//inizializzo variabile che conterrà il numero generato randomicamente
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+//variabile booleana che conserverà l'informazione di vincita o meno
+let win = false;
+
+//ciclo for per i tre tentativi dell'utente
+for (let i = 1; i <= 3; i++) {
+    guess = parseInt(prompt(`sfida la fortuna, ${i}° tentativo`));
+    //condizione di controllo dell'input
+    if (isNaN(guess) || guess <= 0 || guess > 10) {
+        alert("Attenzione, hai sprecato un tentativo con un input non valido");
+        //quindi l'input stesso viene reso un valore nullo
+    }
+    if (guess === randomNumber) {
+        win = true;
+        break;
+    }
+}
+
+//condizione che verifica se l'utente abbia vinto o meno
+if (win === true) {
+    alert("Complimenti, hai vinto un nichelino");
+}
+//se non ha vinto, gli viene proposto di riprovare. Se l'utente accetta, si ricarica la pagina
+else {
+    const tryAgain = prompt("Non è il tuo giorno fortunato. Premi Annulla per uscire, un altro tasto per riprovare")
+    if (tryAgain != null) {
+        document.location.reload();
+    }
+}
---
+++ snack6/README.md
@@ -0,0 +1,2 @@
+#### Fate generare un numero random da 0 a 10 al computer, e chiedete all'utente di inserire un suo numero. Se il numero scelto dall'utente è uguale al numero del computer informate l'utente che ha vinto, altrimenti ha perso.
+
