5e3fedc13cc67ce0deeca819eb18bbf02f59a911
giogiosgr <giovannisgr88@gmail.com>
2024-05-17 11:05:32 +0200

    feat: snack7

--- snack7/JS/main.js
+++ snack7/JS/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+/* Questa parte stampa l'intera tabellina del 2, ma la tengo sotto commento
+
+for (let i = 2; i <= 1000; i += 2) {
+    console.log(i)
+}
+*/
+
+//variabile con il limite deciso dall'utente, da cui estraiamo l'intero
+const range = parseInt(prompt("Inserisci la cifra fino alla quale stampate la tabellina del 2"));
+//condizione di controllo dell'input. Se non è un numero compreso tra 2 e 1000, la pagina viene ricaricata
+if (isNaN(range) || range < 2 || range > 1000) {
+    alert("Attenzione, hai inserito un input non valido");
+    document.location.reload();
+}
+
+//ciclo per la stampa fino al limite deciso dall'utente
+for (let i = 2; i <= range; i += 2) {
+    console.log(i)
+}
+
---
+++ snack7/README.md
@@ -0,0 +1,1 @@
+### Scrivere un programma che stampi la tabellina del 2, fino al numero 1000. Modificate poi il programma in modo che venga chiesto all'utente il numero massimo consentito, e stampare tutta la tabellina del 2 fino al numero inserito.
